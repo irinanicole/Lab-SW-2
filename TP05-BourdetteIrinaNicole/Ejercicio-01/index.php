b80c03a98d72e045153950f41b48ca0d6ae12029
Nicole Bourdette <inbourdette.est@gmail.com>
2024-09-27 18:56:48 -0300

    Primer Commit

--- TP05-BourdetteIrinaNicole/Ejercicio-01/index.php
+++ TP05-BourdetteIrinaNicole/Ejercicio-01/index.php
@@ -3,20 +3,14 @@
     $rutaCss = 'css';
     require_once 'php/encabezado.php';
 ?>
-            <form class="row g-3 needs-validation" action="php/login.php" method="post">
+            <form class="row g-3" action="php/logueo.php" method="post">
                 <section class="col-md-12">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="ejemplo@correo.com" name="correo" required>
-                    <div class="invalid-feedback">
-                        Por favor, introduce un email válido.
-                    </div>
+                    <label for="usu" class="form-label">Usuario</label>
+                    <input type="text" class="form-control" id="usu" name="usuario" >
                 </section>
                 <section class="col-md-12">
                     <label for="password" class="form-label">Contraseña</label>
-                    <input type="password" class="form-control" id="password" placeholder="Contraseña" name="contra" required>
-                    <div class="invalid-feedback">
-                        Por favor, introduce una contraseña.
-                    </div>
+                    <input type="password" class="form-control" id="password" name="contra" required>
                 </section>
                 <section class="col-12 d-flex justify-content-center">
                     <button type="submit" class="btn btn-primary col-4">Iniciar Sesión</button>
--- TP05-BourdetteIrinaNicole/Ejercicio-01/php/logueo.php
+++ TP05-BourdetteIrinaNicole/Ejercicio-01/php/logueo.php
@@ -6,35 +6,44 @@
 
 <section id="respuesta-login">
     <?php
-        if ( !empty($_POST['correo']) && !empty($_POST['contra']) )
+        if ( !empty($_POST['usuario']) && !empty($_POST['contra']) )
         {
-            $correo = trim($_POST['correo']); //elimino espacios en blanco con la funcion "trim()"
+            $usuario = trim($_POST['usuario']); //elimino espacios en blanco con la funcion "trim()"
             $contra = trim($_POST['contra']);
-            
-            echo '<h2 class="text-center">Datos ingresados</h2><br>';
-            echo '<p>Correo: <strong>'.$correo.'</strong></p>';
-            echo '<p>Contraseña: <strong>'.$contra.'</strong></p>';
+           
+            $archivo = fopen('../txt/usuarios.txt','r') ; // abro el archivo para SOLO LECTURA
 
-            $archivo = fopen('../archivo/usuarios.txt','') ;
-
-            $acceso = false;
+            $acceso = false; // en caso de que el usuario y/o contraseña no coincidan
 
             if ($archivo)
             {
                 while (!feof($archivo)) // mientras no se llegue al final del archivo
                 {
-                    $registro = fgets($archivo); // lee una linea del texto por vez (en cada iteracion)
-                    $registroArray = explode(';',trim($registro)); //convierte a un arreglo cada linea del archivo txt
-
+                    $linea = fgets($archivo); // lee una linea del texto por vez (en cada iteracion) hasta llegar al final del archivo
+                    $separados = explode(';',trim($linea)); // convierto a un arreglo cada linea del archivo txt
+                    if ($usuario === $separados[0] && $contra === $separados[1])
+                    {
+                        $acceso = true; // el usuario y la contraseña coinciden
+                        break;
+                    }
                 }
+
+                if ($acceso) // redirige a la pagina 'listado.php' pasándole el dato del usuario en un arreglo '$_GET[]'; 
+                {
+                    header('refresh:0;url=listado.php?usu='.$usuario);
+                }
+                else
+                {
+                    echo '<h2 class="text-center p-3 bg bg-danger bg-opacity-50 rounded">Datos incorrectos</h2>'; // muestra el mensaje de error
+                    header('refresh:3;url=../index.php'); // refresca la pagina durante 3 seg y redirecciona devuelta al index
+                }
+
+                fclose($archivo);
             }
-
-
-
         } 
         else
         {
-            echo '<h2 class="text-center">Datos incorrectos</h2>'; // muestra el mensaje de error
+            echo '<h2 class="text-center">Faltan datos</h2>'; // muestra el mensaje de error
             header('refresh:3;url=../index.php'); // refresca la pagina durante 3 seg y redirecciona devuelta al index
         }
     ?>
