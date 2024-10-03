e5d900f6dd113e61af92c21e193a21f6c2d35aa6
Nicole Bourdette <inbourdette.est@gmail.com>
2024-10-03 10:37:31 -0300

    Primer Commit

--- TP05-BourdetteIrinaNicole/Ejercicio-02/php/procesar.php
+++ TP05-BourdetteIrinaNicole/Ejercicio-02/php/procesar.php
@@ -8,13 +8,11 @@
 
     if ( !empty($_POST['horas_trabajadas']) && !empty($_POST['turno']) && !empty($_POST['dias']) && !empty($_POST['nombre']) ) {
         
+        $nombre = $_POST['nombre'];
         $horas = $_POST['horas_trabajadas'];
         $turno = $_POST['turno'];
-        $nombre = $_POST['nombre'];
 
-        // $lineaDias = implode(';',$_POST['dias']);
-        // $linea =
-
+        echo '<p>Nombre: <strong>'.$nombre.'</strong></p>';
         echo '<p>Horas trabajadas: <strong>'.$horas.'</strong></p>';
         echo '<p>Turno: <strong>'.$turno.'</strong></p>';
         echo '<section class="d-flex flex-row justify-content-center"><table class="table table-bordered table-striped w-75">';
@@ -24,7 +22,8 @@
         echo '<tbody>';
         require_once 'funciones.php';
         $pagoSemanal = 0;
-        foreach ($_POST['dias'] as $dia) {
+        foreach ($_POST['dias'] as $dia)
+        {
             $honorario = pagoDiario ($horas, $dia, $turno);
             echo '<tr><td class="text-start">'.$dia.'</td><td class="w-25 text-end">'.number_format($honorario,2,',','.').'</td></tr>';
             $pagoSemanal = $pagoSemanal + $honorario;
@@ -32,8 +31,43 @@
         echo '<tr class="text-end"><td>Total</td><td>'.number_format($pagoSemanal,2,',','.').'</td></tr>';
         echo '</tbody>';
         echo '</table></section>';
-    }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// TRABAJO DE ARCHIVOS //
+
+        $dias = implode(';',$_POST['dias']);
+
+        $registro = $nombre.';'.$horas.';'.$turno.';'.$dias.';'.$pagoSemanal;
+
+        $registro = trim($registro);
+
+        $carpeta = '../archivos/';
+        if (!file_exists($carpeta))
+        {
+            mkdir($carpeta);
+        }
+        $nombreArchivo = 'enfermeria_honorarios.txt';
+
+        $archivo = fopen($carpeta.$nombreArchivo,'a');
+
+       if ($archivo) // verifica que se haya abierto el archivo
+        {
+            $nuevaLinea = fputs($archivo,$registro.PHP_EOL);
+            if ($nuevaLinea)
+            {
+                echo '<p class="text-center"><strong>Guardado exitoso</strong></p>';
+                fclose($archivo);
+            }
+            else
+            {
+                echo '<p class="text-center"><strong>No se pudo guardar los datos</strong></p>';
+            }
+        }
+        else
+        {
+            echo '<p>No se pudo abrir el archivo</p>';
+        }
+    }
 ?>
 
 <?php
